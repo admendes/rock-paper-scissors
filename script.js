a738470d87d228eb8cc55b21a22c917221601339
Alexandre <alxp.mendes@gmail.com>
2021-03-03 15:51:13 +0000

    buttons

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let options = ["ROCK", "PAPER", "SCISSORS"];
+let playerScore = 0;
+let computerScore = 0;
+let round = 1;
 
 function computerPlay() {
     return options[Math.floor(Math.random() * options.length)];
@@ -14,17 +17,17 @@ function playRound(playerSelection, computerSelection) {
     if ((playerSelection == "ROCK" && computerSelection == "SCISSORS") || 
          (playerSelection == "PAPER" && computerSelection == "ROCK") || 
          (playerSelection == "SCISSORS" && computerSelection == "PAPER")) {
-        return `${playerSelection} vs ${computerSelection} Player wins!`;
+        return `Round ${round++}: ${playerSelection} vs ${computerSelection} Player wins! Score is: ${++playerScore} - ${computerScore}`;
     }
     // Computer wins
     else if ((playerSelection == "SCISSORS" && computerSelection == "ROCK") || 
              (playerSelection == "ROCK" && computerSelection == "PAPER") || 
              (playerSelection == "PAPER" && computerSelection == "SCISSORS")) {
-        return `${playerSelection} vs ${computerSelection} Computer wins!`;
+        return `Round ${round++}: ${playerSelection} vs ${computerSelection} Computer wins! Score is: ${playerScore} - ${++computerScore}`;
     }
     // Tie
     else {
-        return `${playerSelection} vs ${computerSelection} It's a tie!`;
+        return `Round ${round++}: ${playerSelection} vs ${computerSelection} It's a tie! Score is: ${playerScore} - ${computerScore}`;
     }
 }
 
@@ -32,22 +35,34 @@ function wordInArray(arr, word) {
     return (arr.indexOf(word.toUpperCase()) > -1);
 }
 
-function game(){
-    let score = 0;
-    const container = document.querySelector('#container');
+function game(playerSelection){
+    
+    /*const container = document.querySelector('#container');
     const results = document.createElement('div');
     results.classList.add('results');
     results.textContent = 'This is the glorious text-content!';
     container.appendChild(results);
+    */
+    const results = document.querySelector('#results');
+    //results.classList.add('results');
+    results.textContent = playRound(playerSelection, computerPlay());
 
     for (let i=1; i <= 5; i++) {
-        let result = prompt(`Round ${i}!`, "Rock, Paper or Scissors!");
-        results.textContent = playRound(result, computerPlay());
+        //let result = prompt(`Round ${i}!`, "Rock, Paper or Scissors!");
     }
 
 }
 
-document.getElementById("play").addEventListener("click", game);
+function reset() {
+    playerScore = 0;
+    computerScore = 0;
+    round = 1;
+    const results = document.querySelector('#results');
+    results.textContent = "Clear!";
+}
+
+//document.getElementById("play").addEventListener("click", game);
+
 
 /** 
 const container = document.querySelector('#container');
